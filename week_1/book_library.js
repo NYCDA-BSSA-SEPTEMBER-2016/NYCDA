d5c79d4f25e031f86c72d6c63bb9781c06ddbaf7
mistertjen <mistertjen@hotmail.com>
2016-09-23 18:29:12 +0200

    continue week1 work

---
+++ week_1/book_library.js
@@ -0,0 +1,14 @@
+Book library
+
+Create an object that represents a book:
+
+Part 1 Give it a title property with a value that is the title of your book. Give it a body� property which has an array of strings as the value. There should be at least three strings within the array. This array represents the pages of text within your book.
+
+Part 2 Create another object with the same format, but different title and different body.
+
+Part 3 Now, create a function that reads the book by first printing out the title, then printing out each element (page) in the body. For a book with title "Infinite Jest" and pages "Written by David Foster Wallace", "Hal Incandenza is the youngest of the Incandenza children.", and "As a child, he was very precocious.", the output should read as follows:
+
+Title: Infinite Jest
+Page 1: Written by David Foster Wallace
+Page 2: Hal Incandenza is the youngest of the Incandenza children.
+Page 3: As a child, he was very precocious.
--- week_1/fizzbuzz.js
+++ week_1/fizzbuzz.js
@@ -1,12 +1,32 @@
-function startCounting () {
-	for (i = 1; i <= 10; i++) {
-		if (i % 3 === 0) {
+// "Write a program that prints the numbers from 1 to 100. 
+// But for multiples of 3 print Fizz instead of the number and for the multiples of 5 print Buzz. 
+// For numbers which are multiples of both 3 and 5 print FizzBuzz."
 
+var functions = {
+	checkIfModuloZero: function (number, modulo) {
+		if (number % modulo === 0) {
+			return true;
+		} else {
+			return false;
 		}
-			else {
-				console.log()
+	},
+	countTo: function (number) {
+		for (i = 1; i <= number; i++) {
+			if (functions.checkIfModuloZero(i,3) && functions.checkIfModuloZero(i,5)) {
+				console.log('FizzBuzz');
 			}
-	}
-};
+			else if (functions.checkIfModuloZero(i,3)) {
+				console.log('Fizz');
+			}
+			else if (functions.checkIfModuloZero(i,5)) {
+				console.log('Buzz');
+			}
+			else {
+				console.log(i);
+			}
+			// console.log(functions.countTo());
+		}
+	} 
+}
 
-startCounting();
+functions.countTo(100);
