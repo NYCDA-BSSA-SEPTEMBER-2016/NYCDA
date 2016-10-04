ffadacc77949a30254acdd144782588880602edd
mistertjen <mistertjen@hotmail.com>
2016-10-04 22:10:48 +0200

    big cleanup, almost everything works

--- personal-website/includes/main.js
+++ personal-website/includes/main.js
@@ -1,58 +1,49 @@
- Github.userActivity({
-    username: "mistertjen",
-    selector: ".user-2",
-    limit: 20
- });
+(function () {
+	if(typeof Github != 'undefined') {
+		Github.userActivity({
+		    username: "mistertjen",
+		    selector: ".user-2",
+		    limit: 20
+		});
+		// document.getElementsByClassName('button')[0].href = 'mailto:timothytjenalooi@gmail.com';
+	}
+})();
 
- Github.repoProfile({
-    username: 'mistertjen',
-    reponame: 'NYCDA',
-    selector: '.repo-1'
- });
+(function () {
+	if (document.getElementsByClassName('project-container')[0]) {
+		
+		//=AJAX CALL TO GITHUB
+		var githubRepos = new XMLHttpRequest();
+		githubRepos.onreadystatechange = parseGithubRepos;
+		githubRepos.open('GET', 'https://api.github.com/users/mistertjen/repos', true)
+		githubRepos.send();
 
+		//=PARSE DATA
+		function parseGithubRepos () {
+			if (githubRepos.readyState == 4 && githubRepos.status == 200 ) {
+				githubRepos = JSON.parse(githubRepos.responseText);
+				for (var i = 0, repos = {}; i < githubRepos.length; i++) {
+					// in the future update conditions.
+					if (githubRepos[i]['size'] > 1000) {
+						// OBJECT WHERE KEYS ARE THE REPO NAMES
+						repos[githubRepos[i]['name']] = githubRepos[i];
+					};
+				}
 
-if (document.getElementsByClassName('showcase-slider')) {
-	var domSlider = document.getElementsByClassName('showcase-slider');
-	// console.log(domSlider);
-	var githubRepos = new XMLHttpRequest();
-	githubRepos.onreadystatechange = parseGithubRepos;
-	githubRepos.open('GET', 'https://api.github.com/users/mistertjen/repos', true)
-	githubRepos.send();
-
-	function parseGithubRepos () {
-		// console.log('state-changed to ' + githubRepos.readyState);
-		// console.log('works');
-		if (githubRepos.readyState == 4 && githubRepos.status == 200 ) {
-			githubRepos = JSON.parse(githubRepos.responseText);
-			console.log(githubRepos);
-			for (var i = 0, repos = {}; i < githubRepos.length; i++) {
-				// in the future only show repos that have forks > 0;
-				if (githubRepos[i]['size'] > 1000) {
-					repos[githubRepos[i]['name']] = githubRepos[i];
-				};
+				//=DATA TO HTML
+				var counter = 0;
+				for (keys in repos) {
+					document.getElementsByClassName('showcase-project-title')[counter].innerHTML = repos[keys]['name'];
+					document.getElementsByClassName('showcase-project-link')[counter].href = repos[keys]['html_url'];
+					document.getElementsByClassName('showcase-lastest-push')[counter].innerHTML = 'Lastest push on: ' +	String(new Date(repos[keys]['pushed_at'])).slice(4,15);
+					document.getElementsByClassName('showcase-project-languages')[counter].innerHTML = 'Languages: ' + repos[keys]['language'];
+					document.getElementsByClassName('ajax-show')[counter].style.display = "block";
+				}				
+				// fix authentication.
+				// html template.
 			}
-			// console.log(repos);
-			var counter = 0;
-			for (keys in repos) {
-				var projectLastPush = 
-				document.getElementsByClassName('showcase-project-title')[counter].innerHTML = repos[keys]['name'];
-				document.getElementsByClassName('showcase-project-link')[counter].href = repos[keys]['html_url'];
-				document.getElementsByClassName('showcase-lastest-push')[counter].innerHTML = 'Lastest push on: ' +	String(new Date(repos[keys]['pushed_at'])).slice(4,15);
-				document.getElementsByClassName('showcase-project-languages')[counter].innerHTML = 'Languages: ' + repos[keys]['language'];
-			}
-			document.getElementsByClassName('ajax-show')[0].style.visibility = "visible";
-
-					// console.log(githubRepos[i]['name']);
-					// console.log(githubRepos[i]['html_url']);
-					// console.log(githubRepos[i]['language']);
-					// console.log('stars: ' + githubRepos[i]['stargazers_count']);
-					// console.log('watchers: ' + githubRepos[i]['watchers_count']);
-					// console.log('forks: ' + githubRepos[i]['forks_count']);
-					// // save to array[i] or object[i]
-					// // do stuff with that data.
-					// // fix authentication.
+			// I need to build my own API to host images of my projects?
+			// Also need to add descriptions.
 		}
-		// I need to build my own API to host images of my projects?
-		// Also need to add descriptions.
-	}	
-}
+	}
+})();
