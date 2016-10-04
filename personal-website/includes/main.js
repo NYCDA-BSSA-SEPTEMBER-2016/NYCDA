2e1c0e79d757509600cb5750f23e8b730899ee55
mistertjen <mistertjen@hotmail.com>
2016-10-04 17:34:39 +0200

    ajax works, buttons still to fix

--- personal-website/includes/main.js
+++ personal-website/includes/main.js
@@ -20,31 +20,39 @@ if (document.getElementsByClassName('showcase-slider')) {
 	githubRepos.send();
 
 	function parseGithubRepos () {
-		console.log('state-changed to ' + githubRepos.readyState);
-		console.log('works');
+		// console.log('state-changed to ' + githubRepos.readyState);
+		// console.log('works');
 		if (githubRepos.readyState == 4 && githubRepos.status == 200 ) {
 			githubRepos = JSON.parse(githubRepos.responseText);
 			console.log(githubRepos);
-			for (var i = 0, memory = {}; i < githubRepos.length; i++) {
+			for (var i = 0, repos = {}; i < githubRepos.length; i++) {
 				// in the future only show repos that have forks > 0;
 				if (githubRepos[i]['size'] > 1000) {
-					memory[githubRepos[i]['name']] = {
-						
-					};
-					
-
-					console.log(githubRepos[i]['name']);
-					console.log(githubRepos[i]['html_url']);
-					console.log(githubRepos[i]['language']);
-					console.log('stars: ' + githubRepos[i]['stargazers_count']);
-					console.log('watchers: ' + githubRepos[i]['watchers_count']);
-					console.log('forks: ' + githubRepos[i]['forks_count']);
-					// save to array[i] or object[i]
-					// do stuff with that data.
-					// fix authentication.
-				}
+					repos[githubRepos[i]['name']] = githubRepos[i];
+				};
 			}
+			// console.log(repos);
+			var counter = 0;
+			for (keys in repos) {
+				var projectLastPush = 
+				document.getElementsByClassName('showcase-project-title')[counter].innerHTML = repos[keys]['name'];
+				document.getElementsByClassName('showcase-project-link')[counter].href = repos[keys]['html_url'];
+				document.getElementsByClassName('showcase-lastest-push')[counter].innerHTML = 'Lastest push on: ' +	String(new Date(repos[keys]['pushed_at'])).slice(4,15);
+				document.getElementsByClassName('showcase-project-languages')[counter].innerHTML = 'Languages: ' + repos[keys]['language'];
+			}
+			document.getElementsByClassName('ajax-show')[0].style.visibility = "visible";
+
+					// console.log(githubRepos[i]['name']);
+					// console.log(githubRepos[i]['html_url']);
+					// console.log(githubRepos[i]['language']);
+					// console.log('stars: ' + githubRepos[i]['stargazers_count']);
+					// console.log('watchers: ' + githubRepos[i]['watchers_count']);
+					// console.log('forks: ' + githubRepos[i]['forks_count']);
+					// // save to array[i] or object[i]
+					// // do stuff with that data.
+					// // fix authentication.
 		}
 		// I need to build my own API to host images of my projects?
+		// Also need to add descriptions.
 	}	
 }
