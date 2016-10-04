8cca6a5feb8bd175318f26270bb28e91f03d7b61
mistertjen <mistertjen@hotmail.com>
2016-10-04 14:25:33 +0200

    basic ajax, mail button, alt style

--- personal-website/includes/main.js
+++ personal-website/includes/main.js
@@ -9,3 +9,26 @@
     reponame: 'NYCDA',
     selector: '.repo-1'
  });
+
+
+if (document.getElementsByClassName('showcase-slider')) {
+	var domSlider = document.getElementsByClassName('showcase-slider');
+	// console.log(domSlider);
+	var githubRepos = new XMLHttpRequest();
+	
+	githubRepos.onreadystatechange = function () {
+		console.log('state-changed to ' + this.readyState);
+		if (this.readyState == 4 && this.status == 200 ) {
+			githubRepos = JSON.parse(githubRepos.responseText);
+			console.log(githubRepos);
+		}
+	}
+
+	githubRepos.open('GET', 'https://api.github.com/users/mistertjen/repos', true)
+	githubRepos.send();
+
+	
+
+
+	
+}
