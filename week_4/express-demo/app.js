fa98aef87d8ff5b988255631fbd08babc45aa6d5
mistertjen <mistertjen@hotmail.com>
2016-10-17 12:40:25 +0200

    418 I'm a teapot

--- week_4/express-demo/app.js
+++ week_4/express-demo/app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (request,response) => {
+	response.status(418)
 	response.send('Hello Timothy, it is so good to see you in the browser!')
 })
 
 app.listen(8000, () => {
 	console.log('Example app listening on port 2000!')
 })
+
